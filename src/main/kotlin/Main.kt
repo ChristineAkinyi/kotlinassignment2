c153bdd169221eddb1a562503424deee5964d9b0
Christine Akinyi <160093219+ChristineAkinyi@users.noreply.github.com>
2024-02-27 06:48:45 +0300

    Added Changes
    I have removed the println on the return functions since they shouldn't be there
    I have corrected the 'greeting' variable 
    I have also omitted the message in the fun fact and written it only in the main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,19 @@
 fun main() {
-    printName("Hello Christine")
-    modulusTwoNumbers(6, 9)
+    printName("Christine")
+    modulus(6, 9)
     addFourNumbers(2, 8, 13, 278)
-    print("I enjoy watching vlogs and documentaries")
+    println("I enjoy watching vlogs and documentaries")
 }
 
 fun printName(name: String){
-    var name = "Hello Christine"
+    var greeting = "Hello ${name}"
     println(name)
 }
 
-fun modulusTwoNumbers(num1: Int, num2:Int): Int{
+fun modulus(num1: Int, num2:Int): Int{
     var num1 = 6
     var num2 = 9
     var modulus = num1 % num2
-    println(modulus)
     return modulus
 }
 
@@ -24,11 +23,8 @@ fun addFourNumbers(num1: Int, num2: Int, num3: Int, num4: Int): Int{
     var num3 = 13
     var num4 = 278
     var sum = num1+num2+num3+num4
-    println(sum)
     return sum
 }
 
-fun print(message: String){
-    var message = "I love sharing my stories through podcasting"
-    println(message)
-}
+fun funFact(message: String){
+    }
